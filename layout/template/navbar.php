150262b3cedf8cfd1030faaf21f1fa86d8af1ed5
abdulrhman.al <abdulrhman.al@anton-paar.com>
2024-03-26 15:01:41 +0100

    implement it add to cart + sidebar

--- layout/template/navbar.php
+++ layout/template/navbar.php
@@ -23,9 +23,6 @@
         ?>
       </li>
       <li class="nav-item">
-        <a class="nav-link" href="#"><i class="fa fa-cart-plus"></i></a>
-      </li>
-      <li class="nav-item">
         <?php 
          
           if (isset($_SESSION["firstname"]) != null) {
@@ -51,6 +48,60 @@
               echo '</ul>';
           }
         ?>
+      </li>
+     <!-- Sidebar Right -->
+      <li class="nav-item d-flex align-items-center">
+          <a class="nav-link position-relative" href="#" data-bs-toggle="modal" data-bs-target="#shoppingCartModal">
+              <i class="fa fa-cart-plus"></i>
+              <span id="notification-number">
+                  <?php require_once('../cart/cart.php'); getCountId(); ?>
+              </span>
+          </a>
+          <div class="modal fade sidebar-right" id="shoppingCartModal" tabindex="-1" aria-labelledby="shoppingCartModalLabel" aria-hidden="true">
+              <div class="modal-dialog">
+                  <div class="modal-content">
+                      <div class="modal-header">
+                          <h5 class="modal-title" id="shoppingCartModalLabel">Shopping Cart</h5>
+                          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                      </div>
+                      <div class="modal-body">
+                      <?php 
+                        $sql ="SELECT cart.*, products.name, products.img, products.price
+                                FROM cart 
+                                INNER JOIN products ON cart.product_Id = products.id";
+                        $stmt = $db->prepare($sql);
+                        $stmt->execute();
+                        $viewProducts = $stmt->fetchAll();
+                        foreach ($viewProducts as $viewProduct) {
+                        ?>
+                          <div class="container">
+                              <div class="row">
+                                  <div class="col-md-3">
+                                      <img src="../products/products-Image/<?=$viewProduct['img']; ?>" class="img-thumbnail m-1" style="width: 50px; height: 50px;">
+                                  </div>
+                                  <div class="col-md-7">
+                                      <p><?= $viewProduct['name'] ?></p>
+                                      <p><span><?=$viewProduct['amount']?></span> x <span><?=$viewProduct['price']?>$</span></p>
+                                  </div> 
+                                  <div class="col-md-2">
+                                      <form action="../cart/deleteCart.php?id=<?= $viewProduct['id'] ?>" method="post">
+                                          <button type="submit" class="link-offset-2 link-underline link-underline-opacity-0 btn" name="deleteCart">
+                                              <i class="fa fa-times-circle-o"></i>
+                                          </button>
+                                      </form>
+                                  </div>
+                              </div>
+                          </div>
+                        <?php }?>
+                      </div>
+                      <p class="border-top m-2">Subtotal: <span style="float: right;">-----$</span></p>
+                      <div class="modal-footer">
+                          <a href="checkout.php" class="btn btn-primary btn-sm">Checkout</a>
+                          <a href="checkout.php" class="btn btn-primary btn-sm">View Cart</a>
+                      </div>
+                  </div>
+              </div>
+          </div>
       </li>
     </ul>
   </div>
