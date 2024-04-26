402c9b45ff1434bf4b694e5c09ca28b4fd3e4900
abdulrhman.al <abdulrhman.al@anton-paar.com>
2024-04-26 07:36:25 +0200

    Update Data base

--- onlineshop.sql
+++ onlineshop.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.1
+-- version 5.1.1deb5ubuntu1
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1
--- Generation Time: Mar 25, 2024 at 05:40 AM
--- Server version: 10.4.28-MariaDB
--- PHP Version: 8.2.4
+-- Host: localhost:3306
+-- Generation Time: Apr 26, 2024 at 07:35 AM
+-- Server version: 10.6.16-MariaDB-0ubuntu0.22.04.1
+-- PHP Version: 8.1.2-1ubuntu2.14
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -20,6 +20,74 @@ SET time_zone = "+00:00";
 --
 -- Database: `onlineshop`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `cart`
+--
+
+CREATE TABLE `cart` (
+  `id` int(11) NOT NULL,
+  `product_id` int(255) NOT NULL,
+  `user_id` int(255) NOT NULL,
+  `amount` int(255) NOT NULL,
+  `created` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `orders`
+--
+
+CREATE TABLE `orders` (
+  `id` int(11) NOT NULL,
+  `firstName` varchar(255) DEFAULT NULL,
+  `lastName` varchar(255) DEFAULT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  `address` varchar(255) DEFAULT NULL,
+  `zip` varchar(10) DEFAULT NULL,
+  `city` varchar(255) DEFAULT NULL,
+  `orderDate` datetime DEFAULT NULL,
+  `invoice` int(255) NOT NULL,
+  `orderTotal` decimal(10,2) DEFAULT NULL,
+  `user_id` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `orders`
+--
+
+INSERT INTO `orders` (`id`, `firstName`, `lastName`, `email`, `address`, `zip`, `city`, `orderDate`, `invoice`, `orderTotal`, `user_id`) VALUES
+(21, 'Markus', 'Happer', 'markus@anton-paar.com', 'Großklein', '8451', 'Heimshuh', '2024-04-22 08:38:21', 845101, '1411.80', 19),
+(22, 'Markus', 'Happer', 'markus@anton-paar.com', 'Quellenweg 3', '8451', 'Heimshuh', '2024-04-22 08:38:56', 845102, '1667.70', 19),
+(23, 'Markus', 'Happer', 'markus@anton-paar.com', 'Quellenweg 3', '43534', 'Heimshuh', '2024-04-23 10:36:01', 845103, '705.90', 19);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `order_items`
+--
+
+CREATE TABLE `order_items` (
+  `id` int(11) NOT NULL,
+  `order_id` int(11) DEFAULT NULL,
+  `product_id` int(11) DEFAULT NULL,
+  `amount` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `order_items`
+--
+
+INSERT INTO `order_items` (`id`, `order_id`, `product_id`, `amount`) VALUES
+(32, 21, 59, 2),
+(33, 21, 60, 2),
+(34, 22, 59, 3),
+(35, 22, 60, 2),
+(36, 23, 59, 1),
+(37, 23, 60, 1);
 
 -- --------------------------------------------------------
 
@@ -42,8 +110,8 @@ CREATE TABLE `products` (
 --
 
 INSERT INTO `products` (`id`, `name`, `content`, `price`, `created`, `img`, `user_id`) VALUES
-(49, 'American Shorthair111', 'sdfsdfsdfdfd', 3355, '2024-03-21', 'American-Shorthair.jpg', 13),
-(52, 'British Shorthair', 'dfsfd', 32234, '2024-03-21', 'cat.jpg', 13);
+(59, 'Lenovo IdeaPad 3', 'Lenovo IdeaPad 3 CB 14M836 82KN MT8183 / 2 GHz Chrome OS Mali G72 MP3 | 82kn003cge', 255.9, '2024-03-28', 'lenovo.jpg', 18),
+(60, 'Dell Inspiron 3583', 'Dell Inspiron 3583 15 Inch Laptop Intel Celeron - 128GB SSD - 4GB DDR4 - 1.6GHz - Intel UHD Graphi', 450, '2024-03-28', 'dell.jpg', 18);
 
 -- --------------------------------------------------------
 
@@ -57,9 +125,6 @@ CREATE TABLE `users` (
   `lastname` varchar(255) NOT NULL,
   `email` varchar(255) NOT NULL,
   `password` varchar(255) DEFAULT NULL,
-  `phone` varchar(255) NOT NULL,
-  `address` varchar(400) NOT NULL,
-  `img` varchar(500) NOT NULL,
   `role` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
@@ -67,13 +132,34 @@ CREATE TABLE `users` (
 -- Dumping data for table `users`
 --
 
-INSERT INTO `users` (`id`, `firstname`, `lastname`, `email`, `password`, `phone`, `address`, `img`, `role`) VALUES
-(13, 'Abdulrhman', 'Alshalal', 'root@gmail.com', '$2y$10$D09Mu99MbrBq85V0kvoQIOy5tB07tkoDb0x5NShVI05nXgfePQgES', '06648759370', 'Quellenweg 3', 'photo_2021-11-09_03-53-40.jpg', 'Role[\'admin\']'),
-(14, 'Maunel', 'Zitz', 'manuel.zitz@anton-paar.com', '$2y$10$D09Mu99MbrBq85V0kvoQIOy5tB07tkoDb0x5NShVI05nXgfePQgES', '06640000000', 'Großklein', 'cat.jpg', 'Role[\'user\']');
+INSERT INTO `users` (`id`, `firstname`, `lastname`, `email`, `password`, `role`) VALUES
+(18, 'Abdulrhman', 'Alshalal', 'abud@anton-paar.com', '$2y$10$0RwJ2ygxETNPVFErhMjzju6knoYx0W8wI17ywqG9K0UBnJw5w8TfS', 'admin'),
+(19, 'Markus', 'Happer', 'markus@anton-paar.com', '$2y$10$mIZxAOvyGv4AjJimzzAVPeAaDCvx4j74BM.WvI0hvPvAec97yf.r6', 'customer');
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `cart`
+--
+ALTER TABLE `cart`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `product_id` (`product_id`),
+  ADD KEY `user_id` (`user_id`);
+
+--
+-- Indexes for table `orders`
+--
+ALTER TABLE `orders`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `order_items`
+--
+ALTER TABLE `order_items`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `unique_order_product` (`order_id`,`product_id`);
 
 --
 -- Indexes for table `products`
@@ -93,20 +179,50 @@ ALTER TABLE `users`
 --
 
 --
+-- AUTO_INCREMENT for table `cart`
+--
+ALTER TABLE `cart`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=171;
+
+--
+-- AUTO_INCREMENT for table `orders`
+--
+ALTER TABLE `orders`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=24;
+
+--
+-- AUTO_INCREMENT for table `order_items`
+--
+ALTER TABLE `order_items`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=38;
+
+--
 -- AUTO_INCREMENT for table `products`
 --
 ALTER TABLE `products`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=53;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=61;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
 
 --
 -- Constraints for dumped tables
 --
+
+--
+-- Constraints for table `cart`
+--
+ALTER TABLE `cart`
+  ADD CONSTRAINT `fk_cart_products` FOREIGN KEY (`product_id`) REFERENCES `products` (`id`);
+
+--
+-- Constraints for table `order_items`
+--
+ALTER TABLE `order_items`
+  ADD CONSTRAINT `order_items_ibfk_1` FOREIGN KEY (`order_id`) REFERENCES `orders` (`id`);
 
 --
 -- Constraints for table `products`
